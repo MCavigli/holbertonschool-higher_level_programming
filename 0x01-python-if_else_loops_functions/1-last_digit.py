51de7854336d406b88538e990ef2db7130184a7e
Marc Cavigli <mcavigli@gmail.com>
2022-02-22 21:19:02 +0000

    Initial push for task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    lastd = number % -10
+else:
+    lastd = number % 10
+print('Last digit of {} is '.format(number), end="")
+if lastd > 5:
+    print('{} and is greater than 5'.format(lastd))
+elif lastd == 0:
+    print('{} and is 0'.format(lastd))
+elif lastd < 6 and lastd != 0:
+    print('{} and is less than 6 and not 0'.format(lastd))
