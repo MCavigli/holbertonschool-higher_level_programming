0c1c601085e5a0d0e637ac28bcf6d9d096314414
Marc Cavigli <mcavigli@gmail.com>
2022-02-23 05:29:49 +0000

    Removed extraneous print message

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,9 +4,7 @@ from sys import argv
 
 if __name__ == "__main__":
     newsum = 0
-    if len(argv) == 1:
-        print('0')
-    else:
+    if len(argv) > 1:
         for i in range(1, len(argv)):
             newsum += int(argv[i])
     print(newsum)
