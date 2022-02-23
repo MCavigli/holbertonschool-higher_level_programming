f9901e8fb606ad8e7b1f61500322f2b5aad00174
Marc Cavigli <mcavigli@gmail.com>
2022-02-23 02:06:51 +0000

    Fixed missing end statement in print function

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 15 == 0:
-            print('FizzBuzz ')
+            print('FizzBuzz ', end="")
         elif i % 3 == 0:
             print('Fizz ', end="")
         elif i % 5 == 0:
