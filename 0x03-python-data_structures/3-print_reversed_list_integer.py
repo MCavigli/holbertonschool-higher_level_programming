8d01c13e7ac63a7d4b350c5734a887245c6bc169
Marc Cavigli <mcavigli@gmail.com>
2022-03-14 19:44:12 +0000

    Added integer specification for string formatting in print statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,5 @@
 def print_reversed_list_integer(my_list=[]):
     idx = len(my_list) - 1
     for i in my_list:
-        print(my_list[idx])
+        print("{:d}".format(my_list[idx]))
         idx = idx - 1
