35123cbfa498ae09399f69d07d84cba912c06c2d
Marc Cavigli <mcavigli@gmail.com>
2022-03-14 19:46:31 +0000

    Added functionality for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,10 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    idx = len(my_list) - 1
+    if len(my_list) == 0:
+        return
+    else:
+        idx = len(my_list) - 1
     for i in my_list:
         print("{:d}".format(my_list[idx]))
         idx = idx - 1
