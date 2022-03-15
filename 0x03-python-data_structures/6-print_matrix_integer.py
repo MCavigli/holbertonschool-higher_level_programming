1765a562cbb74a3b039e6c3cf59b24b82fe1da1a
Marc Cavigli <mcavigli@gmail.com>
2022-03-15 21:54:39 +0000

    Initial push for task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{} ".format(j), end="")
+            print("{:d}".format(j), end="" if j == i[-1] else " ")
         print()
