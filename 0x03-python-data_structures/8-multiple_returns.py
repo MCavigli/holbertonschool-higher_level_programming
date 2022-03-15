64541d26047a01756793483d5619567efacc6944
Marc Cavigli <mcavigli@gmail.com>
2022-03-15 23:17:28 +0000

    Changed is to ==

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) is 0:
+    if len(sentence) == 0:
         return (len(sentence), None)
     else:
         return (len(sentence), sentence[0])
